3c6db48a77429b0f4d17843c16855f7857f91d34
Tikimilk <pervozvanskaya123@gmail.com>
2025-05-06 14:58:54 +0300

    HW_19

--- playwright.config.ts
+++ playwright.config.ts
@@ -37,20 +37,19 @@ export default defineConfig({
     {
       name: "chromium",
       use: {
-        ...devices["Desktop Chrome"],
-        // , headless: false
+        ...devices["Desktop Chrome"], headless: false
       },
     },
 
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
+    // {
+    //   name: "firefox",
+    //   use: { ...devices["Desktop Firefox"] },
+    // },
 
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
+    // {
+    //   name: "webkit",
+    //   use: { ...devices["Desktop Safari"] },
+    // },
 
     /* Test against mobile viewports. */
     // {
